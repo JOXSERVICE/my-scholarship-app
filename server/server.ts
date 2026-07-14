a41407b1c442e2963e9012be225ec745c3ac77a2
JOXSERVICE <aldalybdh327@gmail.com>
2026-07-14 19:09:28 +0300

    هنا اكتب وصف لما قمت به، مثلا: Connect MongoDB to backend

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Backend with TypeScript is running!");
+});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
